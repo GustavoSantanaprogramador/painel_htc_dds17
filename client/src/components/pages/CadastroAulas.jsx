5ca25549bc7e23eb9ecc9cda04bb3465739f1a58
RAMON DE HOLANDA NASCIMENTO <rholanda@labcetec.net>
2024-10-03 20:05:31 -0300

    tabela

--- client/src/components/pages/CadastroAulas.jsx
+++ client/src/components/pages/CadastroAulas.jsx
@@ -10,11 +10,45 @@ function CadastroAulas() {
   const [instrutor, setInstrutor] = useState('');
   const [unidadeCurricular, setUnidadeCurricular] = useState('');
   const [ambiente, setAmbiente] = useState('');
-  const [infoAulas, setInfoAulas] = useState({});
+
+  //Função temporarária para cdastro de data
+  function formatDataHora(data,hora){
+    const dataHora= `${data}T${hora}:00.000Z`;
+    return dataHora;
+  }
 
   async function cadastrarAula(e) {
     e.preventDefault();
-    console.log(dataAula);
+    //Criando objeto que será encaminhado para o POST da API
+    const infoAula ={
+      data:formatDataHora(dataAula,'00:00'),
+      data_hora_inicio:formatDataHora(dataAula,horaInicio),
+      data_hora_fim:formatDataHora(dataAula,horaFim),
+      turma:turma,
+      instrutor:instrutor,
+      unidade_curricular:unidadeCurricular,
+      ambiente:ambiente,
+      chave:null
+    }
+
+    try {
+      //O POST é usado para inserir elementos na API
+      const resposta = await fetch('http://localhost:5000/aulas',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },body: JSON.stringify(infoAula)
+      });
+
+      if(!resposta.ok){
+        console.log('Erro ao criar aula');
+      }else{
+        alert('Aula cadastrada');
+      }
+
+    } catch (error) {
+      console.error('Erro no cadastro da aula',error)
+    }
   }
   return (
     <div>
@@ -36,27 +70,27 @@ function CadastroAulas() {
           <label className="form-label" htmlFor="">
             Hora Inicio:
           </label>
-          <input className="form-control" type="time" name="" id="" />
+          <input className="form-control" type="time" name="" id="" value={horaInicio} onChange={(e)=>(setHoraInicio(e.target.value))} />
           <label className="form-label" htmlFor="">
             Hora Fim:
           </label>
-          <input className="form-control" type="time" name="" id="" />
+          <input className="form-control" type="time" name="" id="" value={horaFim} onChange={(e)=>(setHoraFim(e.target.value))} />
           <label className="form-label" htmlFor="">
             Turma:
           </label>
-          <input className="form-control" type="text" name="" id="" />
+          <input className="form-control" type="text" name="" id="" value={turma} onChange={(e)=>(setTurma(e.target.value))}/>
           <label className="form-label" htmlFor="">
             Instrutor:
           </label>
-          <input className="form-control" type="text" name="" id="" />
+          <input className="form-control" type="text" name="" id="" value={instrutor} onChange={(e)=>(setInstrutor(e.target.value))}/>
           <label className="form-label" htmlFor="">
             Unidade Curricular:
           </label>
-          <input className="form-control" type="text" name="" id="" />
+          <input className="form-control" type="text" name="" id="" value={unidadeCurricular} onChange={(e)=>(setUnidadeCurricular(e.target.value))}/>
           <label className="form-label" htmlFor="">
             Ambiente:
           </label>
-          <input className="form-control" type="text" name="" id="" />
+          <input className="form-control" type="text" name="" id="" value={ambiente} onChange={(e)=>(setAmbiente(e.target.value))}/>
           <Link className="btn btn-danger mt-2" to="/login">
             Cancelar
           </Link>
