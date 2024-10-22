5ead91ac57ed58a2b0dca2f4baffaf4650d32a10
GUSTAVO DIAS SANTANA <gsantana@labcetec.net>
2024-10-21 21:42:24 -0300

    finalizando  Crud Read, Upadate, Delet

--- server/src/controllers/AulaController.js
+++ server/src/controllers/AulaController.js
@@ -1,6 +1,7 @@
-import { createAula } from '../models/AulaModel.js';
+import { createAula, readAulas, updateAula } from '../models/AulaModel.js';
 
-export async function cadastroAula(req, res) {
+
+export async function cadastroAula(req, res, updateAula) {
   console.log('AulaController cadastroAula');
   //Criando constante aula
   const aula = req.body;
@@ -11,5 +12,47 @@ export async function cadastroAula(req, res) {
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
+  }
+}
+
+export async function mostrandoAulas(req, res) {
+  console.log('AulasController monstrandoAulas');
+  try {
+    const [status, resposta] = await readAulas();
+    res.status(status).json(resposta);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+
+}
+
+export async function atualizadoAula(req, res) {
+  console.log('AulaController atualizandoAula');
+  const { id } = req.params;
+  const aula = req.body;
+
+  try {
+    const [status, resposta] = await updateAula(aula, id);
+    res.status(status).json(resposta);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error);
+
+  }
+}
+
+export async function excluindoAula(req,res) {
+  console.log('AulaController atualizandoAula');
+  const { id } = req.params;
+
+
+  try {
+    const [status, resposta] = await updateAula(aula, id);
+    res.status(status).json(resposta);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error);
+
   }
 }
